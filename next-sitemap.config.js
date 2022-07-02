5f693ea327476e94982c185bb8eadb541aab28e1
sa2taka <sa2taka@gmail.com>
2022-07-02 23:23:55 +0900

    :wrench: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://blog.sa2taka.com',
+  generateRobotsTxt: false,
+  changefreq: 'monthly',
+  outDir: './out',
+};
+
+module.exports = config;
