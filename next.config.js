0b0f16b89825d80ce44a8ca00e49ae6310d9da08
sa2taka <sa2taka@gmail.com>
2022-07-03 18:53:43 +0900

    :sparkles: add pwa fix PWA 対応 #2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,57 @@
-/** @type {import('next').NextConfig} */
 const withLinaria = require('next-linaria');
+const withPWA = require('next-pwa');
+const withOffline = require('next-offline');
 
 const nextConfig = {
   reactStrictMode: true,
+  pwa: {
+    dest: 'out',
+  },
+  workboxOpts: {
+    runtimeCaching: [
+      {
+        urlPattern:
+          '^https?://images.ctfassets.net/xw0ljpdch9v4/.*.(gif|png|jpg|webp)$',
+        handler: 'CacheFirst',
+        method: 'GET',
+      },
+      {
+        urlPattern: '^https?://i.imgur.com/.*.(gif|png|jpg|webp)',
+        handler: 'CacheFirst',
+        method: 'GET',
+      },
+      {
+        urlPattern: '^https://fonts.googleapis.com/.*',
+        handler: 'CacheFirst',
+        method: 'GET',
+      },
+      {
+        urlPattern: '^https://cdnjs.cloudflare.com/.*',
+        handler: 'CacheFirst',
+        method: 'GET',
+      },
+      {
+        urlPattern: '\\.min\\.(css|js)$',
+        handler: 'CacheFirst',
+        method: 'GET',
+      },
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
 };
-
-module.exports = withLinaria({
-  ...nextConfig,
-});
+module.exports = withOffline(
+  withPWA(
+    withLinaria({
+      ...nextConfig,
+    })
+  )
+);
