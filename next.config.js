f153bf7c6825aa5d175a62c6a1ffe570012d98fc
sa2taka <sa2taka@gmail.com>
2022-12-27 23:07:25 +0900

    ⬆️ upgrade libraries

--- next.config.js
+++ next.config.js
@@ -1,52 +1,51 @@
 const withLinaria = require('next-linaria');
-const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  runtimeCaching: [
+    ...runtimeCaching,
+    {
+      urlPattern:
+        '^https?://images.ctfassets.net/xw0ljpdch9v4/.*.(gif|png|jpg|webp)$',
+      handler: 'CacheFirst',
+      method: 'GET',
+    },
+    {
+      urlPattern: '^https?://i.imgur.com/.*.(gif|png|jpg|webp)',
+      handler: 'CacheFirst',
+      method: 'GET',
+    },
+    {
+      urlPattern: '^https://fonts.googleapis.com/.*',
+      handler: 'CacheFirst',
+      method: 'GET',
+    },
+    {
+      urlPattern: '^https://cdnjs.cloudflare.com/.*',
+      handler: 'CacheFirst',
+      method: 'GET',
+    },
+    {
+      urlPattern: '\\.min\\.(css|js)$',
+      handler: 'CacheFirst',
+      method: 'GET',
+    },
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+});
 
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  pwa: {
-    dest: 'public',
-    runtimeCaching: [
-      ...runtimeCaching,
-      {
-        urlPattern:
-          '^https?://images.ctfassets.net/xw0ljpdch9v4/.*.(gif|png|jpg|webp)$',
-        handler: 'CacheFirst',
-        method: 'GET',
-      },
-      {
-        urlPattern: '^https?://i.imgur.com/.*.(gif|png|jpg|webp)',
-        handler: 'CacheFirst',
-        method: 'GET',
-      },
-      {
-        urlPattern: '^https://fonts.googleapis.com/.*',
-        handler: 'CacheFirst',
-        method: 'GET',
-      },
-      {
-        urlPattern: '^https://cdnjs.cloudflare.com/.*',
-        handler: 'CacheFirst',
-        method: 'GET',
-      },
-      {
-        urlPattern: '\\.min\\.(css|js)$',
-        handler: 'CacheFirst',
-        method: 'GET',
-      },
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
 };
 module.exports = withPWA(
   withLinaria({
