3e426f055b35dcd6d881fed17c3711bbfc85dad0
sa2taka <sa2taka@gmail.com>
2024-09-08 17:27:15 +0900

    add rss link

--- packages/blog/src/components/organisms/Footer/index.tsx
+++ packages/blog/src/components/organisms/Footer/index.tsx
@@ -1,10 +1,12 @@
 import sheetStyles from '@blog/styles/shared/sheet.module.css';
 import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faRss } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useMemo } from 'react';
 import { AnimationLink } from '../../atoms/AnimationLink';
 import styles from './index.module.css';
 import { ThemeContext } from '@blog/components/contexts/theme';
+import { BASE_URL } from '@blog/libs/const';
 
 export const Footer: React.FC = () => {
   const { theme } = React.useContext(ThemeContext);
@@ -29,6 +31,18 @@ export const Footer: React.FC = () => {
         </AnimationLink>
         <AnimationLink href="/guide" className={styles.footerLink}>
           <span>当サイト利用について</span>
+        </AnimationLink>
+        <AnimationLink
+          href={`${BASE_URL}/rss.xml`}
+          className={styles.footerLink}
+        >
+          <FontAwesomeIcon
+            icon={faRss}
+            color="#f26522"
+            width={18}
+            style={{ paddingRight: '3px' }}
+          />
+          <span>RSS</span>
         </AnimationLink>
       </div>
     </footer>
